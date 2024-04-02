9fd421f4aedbca71831ff54fa946c4d1ff6f608d
Aje Damilola <ajedamilola2005@gmail.com>
2024-04-02 13:27:41 +0100

    tiny

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ app.use(file())
 app.get("/", (req, res) => { res.send("Express on Vercel"); });
 app.get("/about", (req, res) => { res.send("Another page Vercel"); });
 app.post("/", (req, res) => {
-    res.json({ file: req.files.image })
+    res.json({ file: req.files.image.name })
 })
 const PORT = process.env.PORT || 5000; app.listen(PORT, () => { console.log(`Server is running on port ${PORT}`); });
 module.exports = app;
