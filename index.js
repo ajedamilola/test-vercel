649cbb64d5b796c6d93f66092a12289b86794a35
Aje Damilola <ajedamilola2005@gmail.com>
2024-04-02 13:24:53 +0100

    SMall change

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 const express = require("express");
 const app = express();
+const file = require("express-fileupload")
+app.use(file())
 app.get("/", (req, res) => { res.send("Express on Vercel"); });
 app.get("/about", (req, res) => { res.send("Another page Vercel"); });
+app.post("/", (req, res) => {
+    res.json({ file: req.files.image })
+})
 const PORT = process.env.PORT || 5000; app.listen(PORT, () => { console.log(`Server is running on port ${PORT}`); });
 module.exports = app;
